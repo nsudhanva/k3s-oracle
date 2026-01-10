53574f2964626e28aaa471f37904a2d8ae265e0a
Sudhanva Narayana <nsudhanva@gmail.com>
2026-01-10 11:50:01 -0500

    feat: Implement full OCI Vault integration using Instance Principals

---
+++ tf-k3s/iam.tf
@@ -0,0 +1,17 @@
+resource "oci_identity_dynamic_group" "k3s_nodes" {
+  compartment_id = var.tenancy_ocid
+  name           = "k3s-nodes-dg"
+  description    = "Dynamic group for K3s nodes to access OCI Vault"
+  matching_rule  = "tag.Project.value = 'k3s-oracle-free'"
+}
+
+resource "oci_identity_policy" "k3s_secrets_policy" {
+  compartment_id = var.compartment_ocid
+  name           = "k3s-secrets-read-policy"
+  description    = "Allow K3s nodes to read secrets from Vault"
+
+  statements = [
+    "Allow dynamic-group k3s-nodes-dg to read secret-bundles in compartment id ${var.compartment_ocid}",
+    "Allow dynamic-group k3s-nodes-dg to read secret-bundle-versions in compartment id ${var.compartment_ocid}"
+  ]
+}
